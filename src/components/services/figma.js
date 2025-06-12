8059e0a32a16234c3f4718af06937c7c0e845f23
Ghyaneshdeepz <ghyanesh.co@gmail.com>
2025-06-12 21:38:41 +0530

    lest see 2

--- src/components/services/figma.js
+++ src/components/services/figma.js
@@ -1,15 +1,3 @@
 export async function fetchFigmaData(fileKey) {
-  const url = `https://api.figma.com/v1/files/${fileKey}`;
-
-  const response = await fetch(url, {
-    headers: {
-      "X-Figma-Token": process.env.REACT_APP_FIGMA_TOKEN,
-    },
-  });
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch Figma data");
-  }
-
-  return await response.json();
+  
 }
