5af74b51b2ee8832ac6ce9f516103d7a08ea3206
Ghyaneshdeepz <ghyanesh.co@gmail.com>
2025-06-24 19:44:42 +0530

    data analysis

--- src/services.jsx
+++ src/services.jsx
@@ -1,31 +1,37 @@
 "use client";
 import React from "react";
 import { ContainerScroll } from "../src/components/container-scroll-animation";
-import dash from "./assets/Dash.jpg"
+
 export function Services() {
   return (
-    <div className="flex flex-col overflow-hidden">
+    <div className="flex flex-col sm:block hidden overflow-hidden">
       <ContainerScroll
         titleComponent={
           <>
             <h1 className="text-4xl font-semibold text-black dark:text-white">
-              What I Offer ? <br />
+              What I Offer? <br />
               <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
-                Beautifull <span className="bg-gradient-to-r from-blue-600 via-purple-600 to-pink-500 text-transparent bg-clip-text font-bold italic text-4xl md:text-[6rem] drop-shadow-lg">
-                  designs
+                Designs & {" "}
+                <span className="bg-gradient-to-r from-blue-600 via-purple-600 to-pink-500 text-transparent bg-clip-text font-bold italic text-4xl md:text-[6rem] drop-shadow-lg">
+                  Analysis
                 </span>
-
               </span>
             </h1>
           </>
-        }>
-        <img
-          src={dash}
-          alt="hero"
-          height={720}
-          width={1400}
-          className="mx-auto rounded-3xl object-cover h-full object-left-top"
-          draggable={false} />
+        }
+      >
+        <div className="w-full aspect-video max-w-6xl mx-auto mt-10 rounded-3xl overflow-hidden shadow-xl">
+          <iframe
+            title="Looker Studio Dashboard"
+            width="100%"
+            height="100%"
+            src="https://lookerstudio.google.com/embed/reporting/5c3c8885-cf54-4d79-a1bc-05ef2f9fe81f/page/rvPOF"
+            frameBorder="0"
+            style={{ border: 0 }}
+            allowFullScreen
+            sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+          ></iframe>
+        </div>
       </ContainerScroll>
     </div>
   );
