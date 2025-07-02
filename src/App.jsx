fdd9fd03c5972bacdb159184ede4a826d43e76c3
Ghyaneshdeepz <ghyanesh.co@gmail.com>
2025-07-02 23:28:40 +0530

    3d model location moved hornizaiontal y

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import React from "react";
 import Navbar from './components/navbar';
-
 import Hero from './components/hero';
 import AboutUs from "./components/aboutus";
-import Bglines from "./components/bglines";
-import Working from "./components/working";
-import Aboutme from "./components/aboutme";
 import { Services } from "./services";
 import Skills from "./components/skills";
 import Footer from "./components/footer";
@@ -14,18 +9,12 @@ import MarqueeRibbon from "./components/marquee";
 export default function App() {
   return (
     <div className="text-white min-h-screen relative">
-    
-      
       <div className="fixed inset-0 -z-10">
-        {/* <BackgroundLines className="md:block hidden"/>  */}
       </div>
-
       <Navbar />
       <Hero />
       <AboutUs />
       <Services/>
-      {/* <Working/> */}
-      <Aboutme/>
       <Skills/>
       <MarqueeRibbon/>
       <Contactme/>
--- src/components/figmaconverter.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const FigmaConverter = () => {
-  return (
-  <div></div>
-  );
-};
-
-export default FigmaConverter;
